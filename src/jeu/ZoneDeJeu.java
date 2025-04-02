93d054351215f15029140dec3fd7caba8cc3d999
Axel Carcy <carcyaxel@gmail.com>
2025-04-02 18:58:56 +0200

    Début partie 3 TP3 et Fin TP2
    Commencement de la partie 3 du TP3 et fin du TP2

--- src/jeu/ZoneDeJeu.java
+++ src/jeu/ZoneDeJeu.java
@@ -19,17 +19,32 @@ public class ZoneDeJeu {
 	public int donnerLimitationVitesse() {
 		if (pileLimites.isEmpty())
 			return 200;
-		else if (getSommetPile() instanceof DebutLimite)
+		else if (getSommetLimite() instanceof DebutLimite)
 			return 50;
 		return 200;
 	}
 	
-	public Carte getSommetPile() {
+	public Carte getSommetLimite() {
 		if (pileLimites.isEmpty())
 			return null;
 		
 		return pileLimites.get(pileLimites.size() - 1);
 	}
+	
+	public Bataille getSommetBataille() {
+		if (pileBataille.isEmpty())
+			return null;
+
+		return pileBataille.get(pileBataille.size() - 1);
+	}
+	
+	protected Borne getSommetBorne() {
+		if (pileBornes.isEmpty())
+			return null;
+
+		return pileBornes.get(pileBornes.size() - 1);
+	}
+	
 	
 	public int donnerKmParcourus() {
 		int kmParcourus = 0;
@@ -42,15 +57,51 @@ public class ZoneDeJeu {
 	}
 	
 	public void deposer(Carte carte) {
-		if (carte instanceof Limite limite)
-			pileLimites.add(limite);
-		else if (carte instanceof Bataille bataille)
-			pileBataille.add(bataille);
-		else if (carte instanceof Borne borne)
-			pileBornes.add(borne);
+		if (carte instanceof Limite lim)
+			pileLimites.add(lim);
+		else if (carte instanceof Bataille bat)
+			pileBataille.add(bat);
+		else if (carte instanceof Borne bor)
+			pileBornes.add(bor);
 		else
 			System.out.println("La carte n'a pas pu être ajoutée à une pile");
 	}
 	
+	public boolean peutAvancer() {
+		Carte sommet = getSommetBataille();
+		return pileBataille.isEmpty() && sommet.equals(Cartes.FEU_VERT);
+	}
 	
+	
+	
+	public boolean estDepotFeuVertAutorise() {
+		if (pileBataille.isEmpty())
+			return true;
+		Carte sommet = getSommetBataille();
+		return sommet.equals(Cartes.FEU_ROUGE) || 
+				sommet instanceof Parade && !sommet.equals(Cartes.FEU_VERT);
+	}
+	
+//	public boolean estDepotBorneAutoriser(Borne borne) {
+//		int kmParcourus = donnerKmParcourus();
+//		int kmBorne = borne.getKm();
+//		int limite = 200;
+//		if (getSommetLimite() instanceof DebutLimite)
+//			limite = 50;
+//		return !estBloque() && 
+//				kmBorne <= limite &&
+//				kmParcourus + kmBorne <= 1000;
+//	}
+	
+//	private boolean estBloque() {
+//		if (pileBataille.isEmpty())
+//			return true;
+//		Carte sommetBataille = getSommetBataille();
+//		return sommetBataille.equals(Cartes.FEU_ROUGE) || 
+//				sommetBataille.equals(Cartes.ACCIDENT) ||
+//				sommetBataille.equals(Cartes.CREVAISON) ||
+//				sommetBataille.equals(Cartes.PANNE_ESSENCE);
+//	}
 }
+	
+	
--- src/utils/GestionCartes.java
+++ src/utils/GestionCartes.java
@@ -2,21 +2,25 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.Map;
 import java.util.Random;
+//import java.util.Map;
+//import java.util.LinkedHashMap;
 
 import cartes.Carte;
 
 public class GestionCartes {
+	private static Random random = new Random();
+
+	private GestionCartes() {
+		throw new IllegalStateException("Utility class");
+	}
 
 	public static Carte extraire(List<Carte> liste) {
 		if (liste.isEmpty())
 			throw new IllegalArgumentException("La liste est vide");
 
-		Random random = new Random();
 		int index = random.nextInt(liste.size());
 		return liste.remove(index);
 	}
@@ -25,14 +29,10 @@ public class GestionCartes {
 		if (liste.isEmpty())
 			throw new IllegalArgumentException("La liste est vide");
 
-		Random random = new Random();
 		int index = random.nextInt(liste.size());
 
-		ListIterator<Carte> it = liste.listIterator();
-		Carte carteListe = null;
-		for (int i = 0; i <= index; i++)
-			carteListe = it.next();
-
+		ListIterator<Carte> it = liste.listIterator(index);
+		Carte carteListe = it.next();
 		it.remove();
 		return carteListe;
 	}
@@ -57,81 +57,65 @@ public class GestionCartes {
 		}
 		return true;
 	}
-	
-	// REVENIR PLUS TARD
-//	public static List<Carte> rassembler3(List<Carte> liste) {
-//		ListIterator<Carte> it = liste.listIterator();
-//		List<Carte> listeRassemblee = new ArrayList<>();
-//		while (it.hasNext()) {
-//			Carte elementActuel = it.next();
-//			ListIterator<Carte> it2 = liste.listIterator(it.nextIndex());
-//			
-//		}
-//		return listeRassemblee;
-//	}
-	
+
 	public static List<Carte> rassembler(List<Carte> liste) {
-        List<Carte> resultat = new ArrayList<>();
-        ListIterator<Carte> it = liste.listIterator();
-        
-        while (it.hasNext()) {
-            Carte elementActuel = it.next();
-            if (!resultat.contains(elementActuel)) {
-                resultat.add(elementActuel);
-                ListIterator<Carte> it2 = liste.listIterator(it.nextIndex());
-                while (it2.hasNext()) {
-                    Carte prochainElement = it2.next();
-                    if (it.hasNext() && prochainElement.equals(elementActuel)) {
-                        resultat.add(prochainElement);
-                    }
-                }
-            }
-        }
-        
-        return resultat;
-    }
-	
-	public static List<Carte> rassembler2(List<Carte> liste) {
-		Map<Carte, Integer> occurences = new LinkedHashMap<>();
-		
+		List<Carte> resultat = new ArrayList<>();
 		for (Carte element : liste) {
-			occurences.put(element, occurences.getOrDefault(occurences, 0) + 1);
-		}
-		
-		List<Carte> listeRassembler = new ArrayList<>();
-		for (Map.Entry<Carte, Integer> entry : occurences.entrySet()) {
-			Carte element = entry.getKey();
-			int count = entry.getValue();
-			for (int i = 0; i < count; i++) {
-				listeRassembler.add(element);
+			if (!resultat.contains(element)) {
+				resultat.add(element);
+				Collections.frequency(liste, element);
 			}
 		}
-		return listeRassembler;
+
+		return resultat;
+	}
+
+	// TP5
+//	public static List<Carte> rassembler2(List<Carte> liste) {
+//		Map<Carte, Integer> occurences = new LinkedHashMap<>();
+//
+//		for (Carte element : liste) {
+//			occurences.put(element, occurences.getOrDefault(occurences, 0) + 1);
+//		}
+//
+//		List<Carte> listeRassembler = new ArrayList<>();
+//		for (Map.Entry<Carte, Integer> entry : occurences.entrySet()) {
+//			Carte element = entry.getKey();
+//			int count = entry.getValue();
+//			for (int i = 0; i < count; i++) {
+//				listeRassembler.add(element);
+//			}
+//		}
+//		return listeRassembler;
+//	}
+
+	public static boolean verifierRassemblement(List<Carte> liste) {
+		ListIterator<Carte> it1 = liste.listIterator();
+		for (Carte elementActuel : liste) {
+			Carte elementSuivant = it1.next();
+			if (! elementActuel.equals(elementSuivant)) {
+				verifierListe(liste, it1.nextIndex(), elementSuivant);
+			}
+		}
+		return true;
 	}
 	
-	public static boolean verifierRassemblement(List<Carte> liste) {
-        ListIterator<Carte> it1 = liste.listIterator();
-        
-        while (it1.hasNext()) {
-            Carte elementActuel = it1.next();
-            ListIterator<Carte> it2 = liste.listIterator(it1.nextIndex());
-            
-            while (it2.hasNext()) {
-                Carte prochainElement = it2.next();
-                if (prochainElement.equals(elementActuel))
-                    return false;
-            }
-        }
-        return true;
-    }
-	
-	
-	public static List<Carte> tabToList(Carte[] tab) {
-		List<Carte> liste = new ArrayList<>();
-			Collections.addAll(liste, tab);
+	public static List<Carte> verifierListe(List<Carte> liste, int index, Carte elementActuel) {
+		ListIterator<Carte> it = liste.listIterator(index);
+		for (Carte element : liste) {
+			if (element.equals(elementActuel)) {
+				it.remove();
+			}
+		}
 		return liste;
 	}
-	
+
+	public static List<Carte> tabToList(Carte[] tab) {
+		List<Carte> liste = new ArrayList<>();
+		Collections.addAll(liste, tab);
+		return liste;
+	}
+
 	public static Carte[] listToTab(List<Carte> liste) {
 		Carte[] tab = new Carte[liste.size()];
 		for (int i = 0; i < liste.size(); i++) {
